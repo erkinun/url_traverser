a39761bd3151e0df2e115d4ee4539a1677f5c1d6
lost_mohican <erkinun@gmail.com>
2015-11-24 23:35:10 +0000

    plans are initialized

--- src/main/scala/com/asyaminor/ApplicationMain.scala
+++ src/main/scala/com/asyaminor/ApplicationMain.scala
@@ -3,7 +3,11 @@ package com.asyaminor
 import akka.actor.ActorSystem
 
 object ApplicationMain extends App {
-  val system = ActorSystem("MyActorSystem")
+    
+  println("starting the url traverser")
+  println("ping actors will be url actors!")
+    
+  val system = ActorSystem("UrlActorSystem")
   val pingActor = system.actorOf(PingActor.props, "pingActor")
   pingActor ! PingActor.Initialize
   // This example app will ping pong 3 times and thereafter terminate the ActorSystem - 
