9ff0f3f781f412eb3b30e66304f787bf2c909279
lost_mohican <erkinun@gmail.com>
2016-01-14 22:53:03 +0000

    removed todo

--- src/main/scala/com/asyaminor/MediatorActor.scala
+++ src/main/scala/com/asyaminor/MediatorActor.scala
@@ -17,7 +17,6 @@ class MediatorActor extends Actor with ActorLogging {
   val urlActor = system.actorOf(UrlActor.props, "urlActor")
   val parseActor = system.actorOf(ParserActor.props, "parserActor")
   //TODO measure how long it takes to process a url
-  //TODO write a doc for fatjar with assembly
 
   //get the url message from main
   //send it to URL actors to get html
