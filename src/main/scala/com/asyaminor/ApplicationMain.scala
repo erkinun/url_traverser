398cf7c6c7a1a4b065491a45f5a0d003832d38e4
Erkin Unlu <eunlu@tripadvisor.com>
2016-09-10 23:19:31 +0300

    added help string and testing the remote actor

--- src/main/scala/com/asyaminor/ApplicationMain.scala
+++ src/main/scala/com/asyaminor/ApplicationMain.scala
@@ -2,13 +2,17 @@ package com.asyaminor
 
 import akka.actor.ActorSystem
 import com.asyaminor.MediatorActor.ShutDownMsg
+import com.asyaminor.remote.RemoteUrlActor
 
 import scala.io.StdIn
 
 object ApplicationMain extends App {
 
+  //TODO handle the alive message somehow
+
   val system = ActorSystem("UrlActorSystem")
   val mediator = system.actorOf(MediatorActor.props, "mediator")
+  val remote = system.actorOf(RemoteUrlActor.props, "remote")
 
   //TODO add nice --help section for the app
   //that shows the possible commands and parameters
@@ -51,6 +55,17 @@ object ApplicationMain extends App {
     val url = StdIn.readLine()
 
     url match {
+      case "-h" =>
+        println("q for quit")
+        println("-h for this help screen")
+        println("-test to test remote actor")
+        println("qd to dump the links")
+        println("any other string to traverse the links on it")
+        handleIO()
+      case "-test" =>
+        println("testing the remote actor")
+        remote ! RemoteUrlActor.ALIVE
+        handleIO()
       case "q" =>
         println("quitting...")
         shutDown()
